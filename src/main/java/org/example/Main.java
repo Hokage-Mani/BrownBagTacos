a4c095f1761f9fae4ede1fa4a4d5a39cc26588fa
Hokage-Mani <armaniburton.nyc@gmail.com>
2026-05-27 20:37:47 -0400

    Added a final touch that sparked my interest when learning about GUI. The way that I coded my program If I wanted to use Swing so I didn't want to give up entirely and was able to put java swing into use in the main class since I also designated that to only be used to run the program.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,38 @@
 package org.example;
 
+import javax.swing.*;
+import java.awt.Desktop;
+import java.net.URI;
+
 public class Main {
     public static void main(String[] args) {
-        HomeScreen.main(args);
+        String[] options = {"Run POS System", "Visit Website"};
+        int choice = JOptionPane.showOptionDialog(null,
+                "Welcome to Brown Bag Tacos!\nWhat would you like to launch?",
+                "Brown Bag Tacos Launcher",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.INFORMATION_MESSAGE,
+                null,
+                options,
+                options[0]);
+        if (choice == 0) {
+            System.out.println("Starting the Terminal POS System...\n");
+            HomeScreen.main(args);
+        } else if (choice == 1) {
+            System.out.println("Launching browser...");
+            openWebsite();
 
+        } else {
+            System.out.println("Launcher closed.");
+        }
+    }
+    private static void openWebsite() {
+        try {
+            if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+                Desktop.getDesktop().browse(new URI("https://cupcakesam1121.wixsite.com/brownbagtacos"));
+            }
+        } catch (Exception ex) {
+            System.out.println("Oops! Could not open the website.");
+        }
     }
 }
